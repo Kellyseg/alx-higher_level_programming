54116ab7c96677b3252392366b3d5d54582dd15b
Kellyseg <kekeliseg@gmail.com>
2023-06-08 13:29:19 +0000

    calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
+
 def main():
 	a = 10
 	b = 5
@@ -7,5 +8,6 @@ def main():
 	print("{} - {} = {}".format(a, b, sub(a, b)))
 	print("{} * {} = {}".format(a, b, mul(a, b)))
 	print("{} / {} = {}".format(a,b,  div(a, b)))
+
 if __name__ == "__main__":
     main()
